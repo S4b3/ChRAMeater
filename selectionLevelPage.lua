2fa7f5277a70d866c5f02826c17b629a6a5f790f
LeonardoSano <LeonardoSano1995@gmail.com>
2019-10-14 17:22:53 +0200

    implementata selezione livelli

--- selectionLevelPage.lua
+++ selectionLevelPage.lua
@@ -10,21 +10,14 @@ audio.setVolume( 0.5, { channel=1 } )
 local playBtn -- variabile per il bottone play
 local themeSong -- variabile per la musica di background
 local selectionSound -- variabile per il suono della selezione
-
+local startY = 200
+local levels = {"liv1","liv2"}
 themeSong = audio.loadStream("sounds/menu.mp3")
 selectionSound = audio.loadSound("sounds/select.mp3")
 
-
--- funzione per aprire la scena di Highscores
-local function onHighscoresTap()
-	composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
-	audio.play(selectionSound)
-	return true
-end
-
-local function onPlayTap()
+local function goToLevel(liv)
 	-- vai alla scena level1.lua
-	composer.gotoScene( "selectionLevelPage", { time=800, effect="crossFade" } )
+	composer.gotoScene( liv .. "." .. liv, { time=800, effect="crossFade" } )
 	audio.play(selectionSound)
 	return true	-- indica il tocco successivo
 end
@@ -32,62 +25,34 @@ end
 -- viene chiamato quando la scena non esiste
 function scene:create( event )
 	local sceneGroup = self.view
-
+    local buttons = {}
 	-- mostra un'immagine in background
 	local background = display.newImageRect( "images/background.jpg", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x = 0 + display.screenOriginX 
-	background.y = 0 + display.screenOriginY
-	
-	-- variabile per le caratteristiche della gif di Chrome
-	local sheetChramOptions =
-	{
-		width = 512,
-		height = 512,
-		numFrames = 25
-	}
-	-- variabile immagine Chrome statica
-	local sheetChram = graphics.newImageSheet( "sprites/ChramSprite.png", sheetChramOptions )
-	
-	-- sequenza di immagini consecutive
-	local sequences_chram = {
-		{
-			name = "eatingCram",
-			start = 1,
-			count = 25,
-			time = 860,
-			loopCount = 0,
-			loopDirection = "forward"
-		}
-	}
-    -- variabile immagine Chrome animata
-	local chramEating = display.newSprite( sheetChram, sequences_chram )
-	chramEating.x = display.contentCenterX + 30
-	chramEating.y = display.contentHeight - 900
-	chramEating:scale(2,2)
-	-- variabile chramEating: rotazione(-90)
-	chramEating:play()
-
+    background.y = 0 + display.screenOriginY
+    
 	-- crea immagine logo nella parte superiore della scena
 	local titleLogo = display.newImageRect( "images/Game Title.png", 500, 240 )
 	titleLogo.x = display.contentCenterX
-	titleLogo.y = 200
-    -- crea bottone Play
-	local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, display.contentHeight - 300, native.systemFont, 44 )
-	playButton:setFillColor( 0.82, 0.86, 1 )
-	playButton:addEventListener("tap", onPlayTap)
-	-- crea bottone Highscores
-	local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, display.contentHeight - 220, native.systemFont, 44 )
-	highScoresButton:setFillColor( 0.75, 0.78, 1 )
-    highScoresButton:addEventListener( "tap", onHighscoresTap )
-	
+    titleLogo.y = startY
+    local i = 1 
+    -- crea bottone Play.
 	-- tutti gli oggetti del display devono essere inseriti nel gruppo
 	sceneGroup:insert( background )
-	sceneGroup:insert( titleLogo )
-	sceneGroup:insert( playButton )
-	sceneGroup:insert( highScoresButton )
-	sceneGroup:insert( chramEating )
+    sceneGroup:insert( titleLogo)
+
+    for a,liv in pairs(levels) do
+        function goToLiv()
+            goToLevel(liv)
+        end
+        playButton = display.newText( sceneGroup,liv, display.contentCenterX, startY+80 , native.systemFont, 44 )
+        playButton:setFillColor( 0.82, 0.86, 1 )
+        playButton:addEventListener("tap", goToLiv)
+        table.insert(buttons,playButton)
+        startY = startY +80
+    end   
 end
 
 function scene:show( event )
