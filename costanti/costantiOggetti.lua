f07c705c75d68c70eb9d8103e0025a098175bc08
GiuliaCat <imgiuliacattaneo@gmail.com>
2019-10-15 13:47:28 +0200

    refactor costanti

--- costanti/costantiOggetti.lua
+++ costanti/costantiOggetti.lua
@@ -4,16 +4,19 @@ local bigRamShape = {   (512/2)*2/10,(-288/2)*2/10, (512/2)*2/10,(288/2)*2/10, (
 local smallRamShape = { (512/2)*2/10,(-192/2)*2/10, (512/2)*2/10,(192/2)*2/10, (-512/2)*2/10,(192/2)*2/10, (-512/2)*2/10,(-192/2)*2/10   }
 costanti.levels = {"liv1","liv2","liv3","liv4"}
 
+-- ritorna la forma della ram grande
 function costanti.getBigRamShape()
     return bigRamShape
 end
-
+-- ritorna la forma della ram piccola
 function costanti.getSmallRamShape()
     return smallRamShape
 end
 
+-- configurazione foglio immagine 
 local sheetOptions = {
 
+    -- specifico dove sono posizionati gli oggetti di gioco all'interno del foglio
     frames =
     {
         { -- cache cleaner
@@ -28,13 +31,13 @@ local sheetOptions = {
             width = 512,
             height = 512
         },
-        {--ram big!
+        {-- ram grande
             x = 0,
             y = 1024,
             width = 512,
             height = 288
         },
-        {--ram comune
+        {-- ram piccola
             x = 0,
             y = 1312,
             width = 512,
@@ -43,7 +46,7 @@ local sheetOptions = {
     },
 
 }
-
+-- ritorna il caricamento del foglio immagine in memoria
 function costanti.objectSheet()
     return graphics.newImageSheet("images/GameObjects2.png", sheetOptions)
 end
