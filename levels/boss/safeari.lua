e755759fe10475b95745b2ae8c3cb5e933e5ee46
Emanuele Picariello <emanuele.picariello@sourcesense.com>
2019-10-28 16:16:01 +0100

    aggiunta la funzione shoot anche al livello 3

--- levels/boss/safeari.lua
+++ levels/boss/safeari.lua
@@ -1,13 +1,29 @@
 local composer = require( "composer" )
+local player = require("costanti.player")
+
 
 local safeari = {}
 safeari.show = {}
 safeari.life = 100
+safeari.isDead = false
+
 
 local function movements()
     transition.to(safeari.show, {time=800, x = math.random(0,display.contentWidth), y= math.random(350, 500)})
 end
-
+function safeari.onHit()
+    if(safeari.isDead) then
+        return
+    end
+    print("hit!")
+    safeari.show.hp = safeari.show.hp - 5
+    print(safeari.show.hp)
+    if(safeari.show.hp <= 0 ) then
+        safeari.isDead = true
+        return safeari.isDead
+    end
+    transition.to(safeari.show, {yScale = 1.1, xScale = 1.1, time = 300, onComplete = function () transition.to(safeari.show, {yScale = 1, xScale = 1, time = 300} ) end } )
+end
 local function shoot()
     local projectile = display.newImageRect(safeari.sceneGroup, "images/bosses/safari.png", 100, 100)
     projectile.x = safeari.show.x
@@ -23,10 +39,15 @@ function safeari.safariInit(target, sceneGroup)
     safeari.show = display.newImageRect(sceneGroup, "images/bosses/safari.png", 500, 500)
     safeari.show.x = display.contentCenterX
     safeari.show.y = -100
+    physics.addBody(safeari.show, {radius = safeari.show.contentHeight/2, isSensor = true })
+    safeari.show.hp = 10
     safeari.show:toFront()
     safeari.myName = "Safari"
     transition.to(safeari.show, {time = 3000, y = 350, onComplete =
-         function () Movements = timer.performWithDelay(800, movements, 0) end
+    
+         function () 
+            player.playerChram:addEventListener( "tap" , player.shoot)
+            Movements = timer.performWithDelay(800, movements, 0) end
         })
     
     physics.addBody( safeari.show, {radius = safeari.show.contentHeight/2, isSensor = true})
@@ -35,7 +56,9 @@ function safeari.safariInit(target, sceneGroup)
     timer.performWithDelay(4000, function () ShootTimer = timer.performWithDelay(600, shoot, 0) end)
 end
 function safeari.safariRemove()
-    timer.cancel(ShootTimer)
+    if(ShootTimer ~= nil ) then
+        timer.cancel(ShootTimer)
+    end
     timer.cancel(Movements)
     safeari.show:removeSelf()
 end
