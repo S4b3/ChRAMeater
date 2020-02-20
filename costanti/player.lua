dc5a95354139de469987f142b6645bd246e9a1fd
GiuliaCat <imgiuliacattaneo@gmail.com>
2020-02-20 12:38:44 +0100

    Mancava questo al commit precedente

--- costanti/player.lua
+++ costanti/player.lua
@@ -38,21 +38,24 @@ local function dragPlayerChram( event )
 end
 
 function player.shoot(event)
-    if(costanti.playerState.score == 0 or isStopped) then
-        return
+    if ( event.phase == "began" ) then
+        if(costanti.playerState.score == 0 or isStopped) then
+            return
+        end
+        costanti.playerState.setScore(costanti.playerState.score - 2)
+        local ramShooten = display.newImage(scene, costanti.objectSheet(), 4)
+        physics.addBody( ramShooten, "dynamic", { shape = costanti.smallRamShape } )
+        ramShooten:rotate(90)
+        ramShooten:toBack()
+        ramShooten.myName = "ramShooten"
+        ramShooten:scale(0.27, 0.27)
+        ramShooten.x = player.playerChram.x
+        ramShooten.y = player.playerChram.y
+        table.insert(activeTransitions, transition.to(ramShooten, {y = -10, time = 500, onComplete = function () ramShooten:removeSelf() end }) )
+        costantiSchermo.scoreText.text =  "Score : " .. costanti.playerState.score .. "GB"
+        return true
     end
-    costanti.playerState.setScore(costanti.playerState.score - 2)
-    local ramShooten = display.newImage(scene, costanti.objectSheet(), 4)
-    physics.addBody( ramShooten, "dynamic", { shape = costanti.smallRamShape } )
-    ramShooten:rotate(90)
-    ramShooten:toBack()
-    ramShooten.myName = "ramShooten"
-    ramShooten:scale(0.27, 0.27)
-    ramShooten.x = player.playerChram.x
-    ramShooten.y = player.playerChram.y
-    table.insert(activeTransitions, transition.to(ramShooten, {y = -10, time = 500, onComplete = function () ramShooten:removeSelf() end }) )
-    costantiSchermo.scoreText.text =  "Score : " .. costanti.playerState.score .. "GB"
-    return true
+    return true  --
 end
 
 function player.playerInit(sceneGroup)
